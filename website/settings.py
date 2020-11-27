c09cf367ff06577f33728e3a585e42c694963db3
PeterAwet <peterawet86@gmail.com>
2020-11-27 19:42:35 +0300

    adding procfile and settings adjustment

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o_6dyu1#(l*qg!(^7r-s18o8r&!rf5jw5y9vtw4^u7fph*x6_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dona-digital-marketing.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -58,6 +58,7 @@ CKEDITOR_UPLOAD_PATH = 'ckupload/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,14 +99,25 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database_name',
+        'USER': 'database_username',
+        'PASSWORD': 'database_password',
+        'HOST': 'database_endpoint',
+        'PORT': 'database_port',
+    }
 
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
