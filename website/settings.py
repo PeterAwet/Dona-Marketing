87012652a71e102508820ef415d595ba17acbea2
PeterAwet <peterawet86@gmail.com>
2020-11-28 17:43:02 +0300

    Database Added

--- website/settings.py
+++ website/settings.py
@@ -114,8 +114,12 @@ DATABASES = {
 '''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'marketing'),
+        'USER': 'postgres',
+        'PASSWORD': 'peter123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
