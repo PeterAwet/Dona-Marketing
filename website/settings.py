ae67eb9ee3426660e9cef991042fc75d25f69ed2
PeterAwet <peterawet86@gmail.com>
2020-11-30 17:43:11 +0300

    Added Wahitenoise

--- website/settings.py
+++ website/settings.py
@@ -15,9 +15,9 @@ import psycopg2
 import dj_database_url
 import django_heroku
 
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -184,6 +184,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #crispy form
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
